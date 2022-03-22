272678610566611bf78d74cea87d432623a389b2
Kelebogil <95358859+Kelebogil@users.noreply.github.com>
2022-03-22 16:30:52 +0200

    front and back

---
+++ express-server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path =require ('path');
+const http = require ('http');
+const bodyParser=require ('body-parser')
+
+const api = require('./routes/api');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use('/', api);
+
+
+const port = process.env.PORT ||'4303';
+app.set('port', port);
+
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`THUMA_MINA:${port}`));
